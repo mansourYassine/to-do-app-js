efe4c544a5b586541f4503be6f01bba759d189c7
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-04-19 11:46:27 +0100

    Make the add task window show and disappear when click on buttons

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,24 @@
+let tasks = [];
+
+// Display and hide add new task window
+let newTaskBtn = document.querySelector("main button.new-task");
+let addTaskForm = document.querySelector(".add-form");
+let addTaskFormBtn = document.querySelector(".add-form .content > .buttons .add");
+let cancelTaskFormBtn = document.querySelector(".add-form .content > .buttons .cancel");
+
+newTaskBtn.addEventListener("click", () => {
+    addTaskForm.style = "display: block";
+});
+
+addTaskFormBtn.addEventListener("click", () => {
+    addTaskForm.style = "display: none";
+});
+
+cancelTaskFormBtn.addEventListener("click", () => {
+    addTaskForm.style = "display: none";
+});
+
+// Sidebar Toogle button 
 let mediaQuery = window.matchMedia("(max-width: 991.9px)");
 let sidebar = document.querySelector("aside");
 

92dc6add26388eb43f7144c07bf51bf7c8e7d13b
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-04-22 22:38:24 +0100

    Render Tasks from the tasks array

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,11 @@
-let tasks = [];
+let tasks = [
+    {name: "Eat Breakfast", category: "Health", checked: false},
+    {name: "Go for a walk", category: "Fitness", checked: false},
+    {name: "Read a book", category: "Education", checked: true},
+];
+renderTasks();
 
-// Display and hide add new task window
+// Add new task
 let newTaskBtn = document.querySelector("main button.new-task");
 let addTaskForm = document.querySelector(".add-form");
 let addTaskFormBtn = document.querySelector(".add-form .content > .buttons .add");
@@ -17,6 +22,57 @@ addTaskFormBtn.addEventListener("click", () => {
 cancelTaskFormBtn.addEventListener("click", () => {
     addTaskForm.style = "display: none";
 });
+
+// Display Tasks
+function renderTasks() {
+    let uncompletedTasks = document.querySelector("main > .tasks > .uncompleted-tasks");
+    let completedTasks = document.querySelector("main > .tasks > .completed-tasks");
+
+    uncompletedTasks.innerHTML = "";
+    completedTasks.innerHTML = "";
+
+    let uncompletedTitle = document.createElement("h3");
+    uncompletedTitle.textContent = "All Tasks";
+    uncompletedTasks.appendChild(uncompletedTitle);
+
+    let completedTitle = document.createElement("h3");
+    completedTitle.textContent = "Completed Tasks";
+    completedTasks.appendChild(completedTitle);
+    
+
+    for (let task of tasks) {
+        let divContainer = document.createElement("div");
+        divContainer.classList.add("task");
+    
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        if (task.checked === true) {
+            checkbox.setAttribute("checked", "");
+        }
+        divContainer.appendChild(checkbox);
+    
+        let infoDiv = document.createElement("div");
+        infoDiv.classList.add("info");
+        divContainer.appendChild(infoDiv);
+    
+        let p = document.createElement("p");
+        p.textContent = task.name;
+        infoDiv.appendChild(p);
+        
+        let span = document.createElement("span");
+        span.classList.add("category");
+        span.textContent = task.category;
+        infoDiv.appendChild(span);
+
+        if (task.checked === false) {
+            uncompletedTasks.appendChild(divContainer);
+        } else {
+            divContainer.classList.add("completed");
+            completedTasks.appendChild(divContainer);
+        }
+
+    }
+}
 
 // Sidebar Toogle button 
 let mediaQuery = window.matchMedia("(max-width: 991.9px)");
